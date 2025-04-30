b6fe43e173dd5109c6d71e900d79868ce8315fce
vermavivek951 <vermavivek951@gmail.com>
2025-04-30 16:07:30 +0530

    [Bug Fix] environment based build notification adjusted

--- build_notify.py
+++ build_notify.py
@@ -1,12 +1,14 @@
 import platform
 import os
-import pyttsx3
+
 
 def notify_user(message="Build completed!"):
     try:
         if os.environ.get("GITHUB_ACTIONS") == "true":
             print(f"[CI] Notification: {message}")
             return
+        
+        import pyttsx3
         engine = pyttsx3.init()
         engine.say(message)
         engine.runAndWait()
